c1b71661bd89187c53c22c189f0f0b3556f71340
Alex Ramos <159272348+Katsuhkay@users.noreply.github.com>
2025-11-14 17:59:03 -0300

    sidebar feita

---
+++ src/containers/ListaDeTarefas/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  padding: 0 40px;
+  height: 100vh;
+  overflow-y: scroll;
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`
+export default Container
