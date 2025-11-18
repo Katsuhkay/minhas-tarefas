82ace948443b4b58e077326836e7451c6aad03fa
Alex Ramos <159272348+Katsuhkay@users.noreply.github.com>
2025-11-17 21:18:09 -0300

    edições

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,25 +1,41 @@
 import Tarefa from '../../components/Tarefa'
 import Container from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Nome da tarefa 1',
+    prioridade: 'Importante',
+    status: 'Pendente',
+    descricao: 'Descrição da tarefa 1'
+  },
+  {
+    titulo: 'Nome da tarefa 2',
+    prioridade: 'Urgente',
+    status: 'Concuída',
+    descricao: 'Descrição da tarefa 2'
+  },
+  {
+    titulo: 'Nome da tarefa 3',
+    prioridade: 'Baixa',
+    status: 'Atrasada',
+    descricao: 'Descrição da tarefa 3'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            titulo={t.titulo}
+            prioridade={t.prioridade}
+            status={t.status}
+            descricao={t.descricao}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
